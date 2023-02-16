7abd484edb2f4c22f7bbaea76c0da2823e39fe50
Lwimix <martilwinx@gmail.com>
2023-02-16 12:23:09 -0800

    Variable type sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * prints size of variables
+ *
+ * Return: 0 if program was successful and any other number for errors
+ */
+int main(void)
+{
+	printf("Size of a char: %u byte(s)\n", sizeof(char));
+	printf("Size of a int: %u byte(s)\n", sizeof(int));
+	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %u byte(s)\n", sizeof(float));
+	return (0);
+}
