be150294e994e5e171e0080cdd0cf8c2ce945a6c
Lwimix <martilwinx@gmail.com>
2023-02-16 07:25:25 -0800

    Using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,12 @@
 /**
  * main - function that executes program logic.
  *
- * @return - offers a return value to indicate whether a program has run successfully or not.
+ * @return - offers a return value to indicate whether
+ * * a program has run successfully or not.
  */
 int main(void)
 {
 	printf("with proper grammar,"
-	"but the outcome is a piece of art,\n");
+	" but the outcome is a piece of art,\n");
 	return (0);
 }
