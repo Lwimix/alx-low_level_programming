5763a9538c5872c336b17e5410e3bbf0d44dd959
Lwimix <martilwinx@gmail.com>
2023-02-16 02:09:50 -0800

    Betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/* main - entry point
+ *
+ * return - end point 0(Success)
+ */
 int main(void)
 {
-	char str[55] = "\"Programming is like building a multilingual puzzle\n";
+	char str[55] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
 	return (0);
 }
