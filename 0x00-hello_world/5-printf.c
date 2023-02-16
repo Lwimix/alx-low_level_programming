30312b560229a3219792c773acf1ecc738197cdc
Lwimix <martilwinx@gmail.com>
2023-02-16 06:12:45 -0800

    Using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function that executes program logic.
+ *
+ * @return - offers a return value to indicate whether a program has run successfully or not.
+ */
+int main(void)
+{
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
