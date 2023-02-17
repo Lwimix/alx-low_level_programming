12b49bb9325f6db51dae800186df280f1c4b47ce
Lwimix <martilwinx@gmai.lccom>
2023-02-17 19:51:18 +0300

    The alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,11 +5,11 @@
  *
  * Return: 0 when successful
  */
-int main (void)
+int main(void)
 {
 	int n = 'a';
 
-	while( n <= 'z' && n != '\0')
+	while (n <= 'z' && n != '\0')
 	{
 		putchar(n);
 		n++;
