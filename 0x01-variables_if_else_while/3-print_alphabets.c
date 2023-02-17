f68db6605595b4fe92829c6314d7af6108bf132c
Lwimix <martilwinx@gmai.lccom>
2023-02-17 20:05:22 +0300

    The two sides of the alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 for SUCCESS
+ */
+int main(void)
+{
+	int m = 'a';
+	int n = 'A';
+	while (m <= 'z' && m != '\0')
+	{
+		putchar(m);
+		m++;
+	}
+	while (n <= 'Z' && n != '\0')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
