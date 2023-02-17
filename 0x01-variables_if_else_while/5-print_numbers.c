45b73eb45b49be85896234a8fb3a333c51bc0217
Lwimix <martilwinx@gmai.lccom>
2023-02-17 21:00:34 +0300

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,13 +3,13 @@
 /**
  * main - entry point
  *
- * Return - 0 for SUCCESS
+ * Return: - 0 for SUCCESS
  */
 int main(void)
 {
 	int num = 0;
 
-	for(num = 0; num <= 9; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		printf("%d", num);
 	}
