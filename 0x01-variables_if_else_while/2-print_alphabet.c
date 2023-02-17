0a9fbd2ed1a60ba5484be01686b7a6c12347cd1b
Lwimix <martilwinx@gmai.lccom>
2023-02-17 19:44:14 +0300

    The alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 when successful
+ */
+int main (void)
+{
+	int n = 'a';
+	while ( n <= 'z' && n != '\0')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
