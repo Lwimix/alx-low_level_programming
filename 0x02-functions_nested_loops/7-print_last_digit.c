fef7db5262e610d441722ee1c4ece1928433e157
Lwimix <martilwinx@gmail.com>
2023-02-22 01:49:28 +0300

    ten

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - last digit
+ * @u - number
+ *
+ * Return: last digit
+ */
+int print_last_digit (int u)
+{
+	int last;
+	last = u % 10;
+	if (last < 0)
+	{
+		last = last * -1;
+		_putchar(last + '0');
+		return (last);
+	}
+	_putchar(u + '0');
+	return u;
+}
