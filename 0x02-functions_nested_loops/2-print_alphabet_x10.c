6d646fee5fb2fb410b8781cf64a84ef2f2858980
Lwimix <martilwinx@gmail.com>
2023-02-21 08:17:00 -0800

    To my pc

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - check code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int cnt = 0;
+	char u = 'a';
+
+	while(u <= 'z')
+	{
+		_putchar(u);
+		u++;
+	}
+	_putchar('\n');
+	return (0);
+}
