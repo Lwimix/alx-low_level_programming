31bd7c392615aa50d9c2c464cba0e39af2b45a54
Lwimix <martilwinx@gmail.com>
2023-02-21 21:37:26 +0300

    Compare the small

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * lower_main - looks for lowercase alphabet
+ * _islower - looks for lowercase alphabet
+ * @c: character to be checked
  *
  * Return: 1 for success.
  */
 int _islower(int c)
 {
-	if ('a'<= c && c <= 'z')
+	if ('a' <= c && c <= 'z')
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
 }
