509a616d0bde9beaca402e51c2a0b001cc2944e9
Lwimix <martilwinx@gmail.com>
2023-02-23 00:33:45 +0300

    Big

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 	
-	if (a > b && a > c)
+	if ((a - b) > 0 || (a - c) > 0)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if ((b - a) > 0 && (b - c) > 0)
 	{
 		largest = b;
 	}
