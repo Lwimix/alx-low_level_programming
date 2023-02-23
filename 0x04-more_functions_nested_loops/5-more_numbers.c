e274ad8eed71780b255267275e92f4b3f02e822b
Lwimix <martilwinx@gmail.com>
2023-02-23 17:36:25 +0300

    Multiply

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,15 +9,23 @@ void more_numbers(void)
 {
 	int in_count;
 
-	for (in_count = 0; in_count <= 10; in_count++)
+	for (in_count = 0; in_count < 10; in_count++)
 	{
 		int count;
 
 		for (count = 0; count <= 14; count++)
 		{
+			while (count > 9 && count <=14)
+			{
+				int you;
+
+				for (you = 10; you <= 14; you++)
+				{
+					_putchar(you + 38);
+				}
+			}
 			_putchar(count + '0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
