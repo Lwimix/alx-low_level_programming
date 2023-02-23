bcc0d55e2454ce8dcc06afec45afd457e575287c
Lwimix <martilwinx@gmail.com>
2023-02-24 00:57:35 +0300

    Tree

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,29 +1,18 @@
 #include "main.h"
 
-/**
- * print_triangle - prints a triangle
- * @size: size of triangle
- *
- * Return: 1 for SUCCESS, 0 otherwise
- */
 void print_triangle(int size)
 {
-	int in_count;
-
-	for (in_count = 0; in_count < size; in_count++)
+	int i, j;
+	if (size <= 0)
 	{
-
-		int count;
-
-		for (count = 0; count < size; count++)
-		{
-			int space = 
-			_putchar('#');
-		}
 		_putchar('\n');
 	}
-	if(in_count == '\0')
+	for (i = 0; i < size; i++)
 	{
+		for (j = 0; j < i + 1; j++)
+		{
+			_putchar('#');
+		}
 		_putchar('\n');
 	}
 }
