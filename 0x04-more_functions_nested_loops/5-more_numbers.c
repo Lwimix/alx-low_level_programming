743ab1cd7a7c52cce0cfc3dc2054e7578a6a7631
Lwimix <martilwinx@gmail.com>
2023-02-23 22:25:04 +0300

    Fizz

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,31 +1,29 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers (0-14)
+ * print_most_numbers - prints numbers (0-14)
  *
  * Return: 1 for SUCCESS, 0 otherwise
  */
 void more_numbers(void)
 {
-	int in_count;
+	int count;
 
-	for (in_count = 0; in_count < 10; in_count++)
+	count = 0;
+	while (count <= 14)
 	{
-		int count;
-
-		for (count = 0; count <= 14; count++)
+		if (count <= 9)
 		{
-			while (count > 9 && count <=14)
-			{
-				int you;
-
-				for (you = 10; you <= 14; you++)
-				{
-					_putchar(you + 38);
-				}
-			}
 			_putchar(count + '0');
+			count++;
 		}
-		_putchar('\n');
+		else
+		{
+			int made = 49 + 49;
+
+			_putchar(made + '0');
+			count++;
+		}
 	}
+	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
+
 /**
  * main - entry point
  *
- * Return: 0 for SUCCESS
+ * Return: Always 0
  */
 int main(void)
 {
-	int num;
-
-	for (num = 1; num <= 100; num++)
+	int num = 1;
+	while (num <= 100)
 	{
-		if (num % 3 == 0 && num % 5 != 0)
+		if (num % 3 == 0 && num % 5 == 0)
 		{
-			printf("Fizz");
+			printf("FizzBuzz ");
 		}
-		else if (num % 5 == 0 && num % 3 != 0)
+		else if (num % 3 == 0)
 		{
-			printf("Buzz");
+			printf("Fizz ");
 		}
-		else if (num % 3 == 0 && num % 5 == 0)
+		else if (num % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf("Buzz ");
 		}
 		else
 		{
-			printf("%d", num);
+			printf("%d ", num);
 		}
-		printf(" ");
+		num++;
 	}
 	printf("\n");
 	return (0);
