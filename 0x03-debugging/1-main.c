95c18ced81090d6714c5fec7be684f16862af7d5
Lwimix <martilwinx@gmail.com>
2023-02-23 17:09:54 +0300

    Call the debug king cause I be degugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int i;
-	
+	/* Comments are in blue */
 	printf("Infinite loop incoming :(\n");
 	/**
 	 * i = 0;
@@ -19,6 +19,6 @@ int main(void)
 	 * }
 	 */
 	printf("Infinite loop avoided! \\o/\n");
-	
+	/* They make this look colorful */
 	return (0);
 }
