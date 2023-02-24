c8ffcd2211e0ede5227833e7e1a24f2f090aba0d
Lwimix <martilwinx@gmail.com>
2023-02-25 01:58:07 +0300

    NUm

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,9 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j <= 14 ; j++)
 		{
-			if (j > 9)
+			if (j >= 10)
 			{
 				_putchar('0' + (j / 10));
 				_putchar('0' + (j % 10));
@@ -27,4 +27,3 @@ void more_numbers(void)
 		_putchar('\n');
 	}
 }
-
