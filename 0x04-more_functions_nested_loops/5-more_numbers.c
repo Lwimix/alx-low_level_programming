cec391762363b190601f2e23c3e81c2d00a121a0
Lwimix <122812384+Lwimix@users.noreply.github.com>
2023-02-25 01:01:21 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,16 +10,15 @@ void more_numbers(void)
 	int count;
 
 	count = 0;
-	while (count <= 14)
+	for (count = 0; count <= 14; count++)
 	{
 		if (count <= 9)
 		{
 			_putchar(count + '0');
-			count++;
 		}
 		else
 		{
-			int made = 49 + 49;
+			int made[] = 49 + 49;
 
 			_putchar(made + '0');
 			count++;
