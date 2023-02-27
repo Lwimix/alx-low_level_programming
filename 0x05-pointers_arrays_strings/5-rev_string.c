f4b387d4051eef508428bce7e1eec822b8470907
Lwimix <martilwinx@gmail.com>
2023-02-28 00:51:06 +0300

    Reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,24 +2,7 @@
 #include <unistd.h>
 
 /**
- * rev_string - reverses string showing original
- * @s: pointer variable
- *
- * Return: nothing.
- *
-void rev_string(char *s)
-{
-	int i = 0;
-	while (s[i] != '\0')
-	{
-		_putchar(s[i]);
-		i++;
-	}
-	_putchar('\n');
-}*/
-
-/**
- * print_rev - prints a string in reverse
+ * rev_string - prints a string in reverse
  * @s: pointer variable to string
  *
  * Return: nothing.
@@ -29,7 +12,7 @@ void rev_string(char *s)
 {
 	int len = 0;
 	char *ptr = s;
-	
+
 	while (*ptr != '\0')
 	{
 		len++;
