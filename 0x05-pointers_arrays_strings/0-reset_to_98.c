e91b85b9f361b189da67ad8fa0b570866b076ff5
Lwimix <martilwinx@gmail.com>
2023-02-27 12:55:25 +0300

    Only 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes variable outside function
+ * @*n: pointer to variable to be changed
+ *
+ * Return: nothing.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
