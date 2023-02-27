d263ce9637be09615722d010541936e4e5ab13eb
Lwimix <martilwinx@gmail.com>
2023-02-27 23:36:07 +0300

    Reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,9 +10,14 @@ void print_rev(char *s)
 {
 	int index = 0;
 
-	while (s[index - 1] != s[index])
+	while (s[index] != '\0')
+	{
+		index++;
+	}
+	while (index >= 0)
 	{
 		_putchar(s[index]);
 		index--;
 	}
+	_putchar('\n');
 }
