06f54620ad44877fa4319decdf79fe2d59297625
Lwimix <martilwinx@gmail.com>
2023-03-02 09:59:53 +0300

    AlPhaBet

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize letter after special character
+ * @strn: pointer variable
+ *
+ * Return: capitalized string
+ */
+char *cap_string(char *strn)
+{
+	int u = 0;
+
+	while (strn[u] != '\0')
+	{
+		if (strn[u] == ',' || strn[u] == ';' || strn[u] == '.' ||
+				strn[u] == '!' || strn[u] == '\?' ||
+				strn[u] == '\"' || strn[u] == '(' ||
+				strn[u] == ')' || strn[u] == '{' ||
+				strn[u] == '}' || strn[u] == ' ' ||
+				strn[u] == '\n')
+		{
+			u++;
+			if (strn[u] >= 97 && strn[u] <= 122)
+			{
+				strn[u] = strn[u] - 32;
+			}
+		}
+		else if (strn[u] == '\t')
+		{
+			strn[u] = ' ';
+		}
+		else
+		{
+			u++;
+		}
+	}
+	return (strn);
+}
