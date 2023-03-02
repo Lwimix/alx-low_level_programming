7bb1faa27831faaef383426c23c5770d75e40ba8
Lwimix <martilwinx@gmail.com>
2023-03-02 08:04:30 +0300

    Upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,15 +8,14 @@
  */
 char *string_toupper(char *s)
 {
-	int i = 97, count = 0;
+	int count = 0;
 
-	while (i < 123)
+	while (s[count] != '\0')
 	{
 		if (s[count] >= 97 && s[count] <= 122)
 		{
 			s[count] = s[count] - 32;
 		}
-		i++;
 		count++;
 	}
 	return (s);
