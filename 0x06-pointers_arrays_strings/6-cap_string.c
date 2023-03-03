3984c1a496a9eaa41c85ab30ad539f05fb67f435
Lwimix <martilwinx@gmail.com>
2023-03-04 01:05:02 +0300

    cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -43,35 +43,52 @@ char *cap_string(char *strn)
 }*/
 #include <stddef.h>
 
-char* cap_string(char* str) {
-	    int i = 0;
-	        int cap_next = 1;
-		    while (str[i] != '\0') {
-			            if (cap_next && str[i] >= 'a' && str[i] <= 'z') {
-					                str[i] -= ('a' - 'A');
-							        }
-				            cap_next = 0;
-					            switch (str[i]) {
-							                case ' ':
-										            case '\t':
-										            case '\n':
-										            case ',':
-										            case ';':
-										            case '.':
-										            case '!':
-										            case '?':
-										            case '"':
-										            case '(':
-										            case ')':
-										            case '{':
-										            case '}':
+char* cap_string(char* str)
+{
+	int i = 0;
+	int cap_next = 1;
+	while (str[i] != '\0')
+	{
+		if (cap_next && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= ('a' - 'A');
+		}
+		cap_next = 0;
+		switch (str[i])
+		{
+		case ' ':
+		
+		case '\t':
+		
+		case '\n':
+		
+		case ',':
+		
+		case ';':
+		
+		case '.':
+		
+		case '!':
+		
+		case '?':
+		
+		case '"':
+		
+		case '(':
+		
+		case ')':
+		
+		case '{':
+		
+		case '}':
 										                cap_next = 1;
-												                break;
-														            default:
-														                break;
-																        }
-						            i++;
-							        }
-		        return str;
+												break;
+		
+		default:
+												break;
+		}
+		i++;
+	}
+	return str;
 }
 
