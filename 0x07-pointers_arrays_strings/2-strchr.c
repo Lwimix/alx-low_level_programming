6d71dbed197c475f442257f8b5c22e1080a03c42
Lwimix <martilwinx@gmail.com>
2023-03-07 01:54:34 +0300

    string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,7 +8,7 @@
  *
  * Return: string beginning from char
  */
-char *_strchr(char *s, char c)
+/*char *_strchr(char *s, char c)
 {
 	int v;
 
@@ -20,4 +20,21 @@ char *_strchr(char *s, char c)
 		}
 	}
 	return (NULL);
+}*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return s;
+		}
+		s++;
+	}
+	if (c == '\0')
+	{
+		return s;
+	}
+	return NULL;
 }
