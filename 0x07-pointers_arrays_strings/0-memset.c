5f6e61514fc3990c00c72ac935c073d0df1e17ab
Lwimix <martilwinx@gmail.com>
2023-03-06 21:16:56 +0300

    no mem

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - replace values in memory
+ * @s: buffer
+ * @b: replacement
+ * @n: no of times to replace
+ *
+ * Return: replaced buffer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i, x, u = 0;
+	x = n;
+	
+	for (i = 0; i < x; i++)
+	{
+		s[i] = b;
+		s[u] = s[i];
+		u++;
+	}
+	return (s);
+}
