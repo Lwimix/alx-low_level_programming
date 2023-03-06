402e34efea4c6bd1dafe54f76eb68cc87cffba1f
Lwimix <martilwinx@gmail.com>
2023-03-07 02:01:41 +0300

    Mp

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,7 +9,7 @@
  *
  * Return: changed string
  */
-char *_strstr(char *haystack, char *needle)
+/*char *_strstr(char *haystack, char *needle)
 {
 	int i, u, j, l;
 
@@ -26,4 +26,23 @@ char *_strstr(char *haystack, char *needle)
 		}
 	}
 	return (NULL);
+}*/
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		char *h = haystack;
+		char *n = needle;
+		while (*n != '\0' && *h == *n)
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+		{
+			return haystack;
+		}
+		haystack++;
+	}
+	return NULL;
 }
