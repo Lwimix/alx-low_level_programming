aa7462d5050444e76e0e29341ac1ac432e51ad16
Lwimix <martilwinx@gmail.com>
2023-03-10 18:40:02 +0300

    Arr

---
+++ 0x0A-arc-argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: the arguments themselves
+ *
+ * Return: 0 for SUCCESS
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+	return (0);
+}
