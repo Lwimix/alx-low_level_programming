38926870b7854c02fb9aa6031263666ac9ab9081
Lwimix <martilwinx@gmail.com>
2023-03-11 01:31:26 +0300

    no way

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 /**
  * main - entry
@@ -10,20 +11,22 @@
  */
 int main(int argc, char *argv[])
 {
-	int sum = 0, count = 0;
+	int sum = 0, count = 1;
 
 	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
-	if (atoi(argv[count]) >= 'a' && atoi(argv[count]) <= 'z')
-	{
-		printf("Error\n");
-	}
-	if ()
 	for(; count < argc; count++)
 	{
+		char *endptr;
+		long int num = strtol(argv[count], &endptr, 10);
+		if (*endptr != '\0' || num > INT_MAX || num < INT_MIN)
+		{
+			printf("Error\n");
+			return (1);
+		}
 		sum += atoi(argv[count]);
 	}
 	printf("%d\n", sum);
