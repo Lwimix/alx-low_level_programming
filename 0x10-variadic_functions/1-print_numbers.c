4a0f7a4baf9a574aa3b7ed44227aec5b239cba63
Lwimix <martilwinx@gmail.com>
2023-03-23 22:25:26 +0300

    All the King's men

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,13 +17,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(call, n);
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
-		if (separator == NULL)
-			printf("%d ", va_arg(call, int));
-		printf("%d%c ", va_arg(call, int), *separator);
+		printf("%d", va_arg(call, int));
+		if ((i < n - 1) && separator != NULL)
+			printf("%d%c ", va_arg(call, int), *separator);
 	}
-	printf("%d\n", va_arg(call, int));
+	printf("\n");
 	va_end(call);
 
 }
