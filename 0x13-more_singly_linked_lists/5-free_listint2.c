e1ad41d70519b1be0ddfc1af102f662551b4dca6
Lwimix <martilwinx@gmail.com>
2023-04-04 21:43:34 +0300

    Could we link up

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,14 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *current = *head, *next;
-
+	
+	if (*head == NULL)
+		return;
 	while (current != NULL)
 	{
 		next = current->next;
 		free(current);
 		current = next;
 	}
+	*head = NULL;
 }
