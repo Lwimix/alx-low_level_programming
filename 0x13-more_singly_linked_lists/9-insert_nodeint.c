a136ef91577278e10106aef175b839a0f774e317
Lwimix <martilwinx@gmail.com>
2023-04-05 01:48:19 +0300

    Lost links

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,6 +23,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
+	if (copy == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	if (current == NULL || current->next == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 	new->next = current->next;
 	current->next = new;
 	return (new);
