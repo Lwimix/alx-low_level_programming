0db32f2742b16256d07d9b5eced1a48338ad0e19
Lwimix <martilwinx@gmail.com>
2023-04-10 23:18:45 +0300

    I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,9 +18,9 @@ int create_file(const char *filename, char *text_content)
 	int fd, num_char;
 	size_t len;
 	char *str;
-	
+
 	len = strlen("chmod 600 ");
-	str = (char *)malloc((strlen(filename) + len +1));
+	str = (char *)malloc((strlen(filename) + len + 1));
 	if (str == NULL)
 		return (0);
 	strcpy(str, "chmod 600 ");
@@ -44,14 +44,14 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 	{
 		if (errno == ENOENT)
-			fd = open(filename, O_CREAT|O_RDWR);
+			fd = open(filename, O_CREAT | O_RDWR);
 		if (fd == -1)
 			return (-1);
 	}
 	num_char = write(fd, text_content, strlen(text_content));
 	if (num_char == 0)
 		return (-1);
-	else 
+	else
 		return (1);
 
 }
