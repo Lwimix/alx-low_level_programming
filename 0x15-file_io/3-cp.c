a88316af62e2adb49987593dfc00ba02b41dafa0
Lwimix <martilwinx@gmail.com>
2023-04-13 01:11:38 +0300

    Late night Changes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,7 +14,7 @@
  */
 int main(int argc, char **argv)
 {
-	int fd, num_char, fd2, close_err, close_err2, closer, num_read;
+	int fd, num_char, fd2, close_err, close_err2, num_read;
 	char buf[buf_size];
 
 	if (argc != 3)
@@ -24,31 +24,23 @@ int main(int argc, char **argv)
 	}
 	fd = open(argv[1], O_RDONLY);/*Open file*/
 	if (fd == -1)
-	{
-		dprintf(2, "Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		err_98_print(argv);
 	fd2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (fd2 == -1)
-		err_print(argv);
+		err_99_print(argv);
 	while ((num_read = read(fd, buf, buf_size)) > 0)
 	{
 		if (num_read == -1)
-		{
-			dprintf(2, "Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
+			err_98_print(argv);
 		num_char = write(fd2, buf, num_read);/*Write to new file*/
 		if (num_char == -1)
-			err_print(argv);
+			err_99_print(argv);
 	}
 	close_err = close(fd);
+	if (close_err == -1)
+		err_100_print(fd);
 	close_err2 = close(fd2);
-	closer = (close_err == -1) ? fd : fd2;
-	if (close_err == -1 || close_err2 == -1)
-	{
-		dprintf(2, "Error can't close fd %d", closer);
-		exit(100);
-	}
+	if (close_err2 == -1)
+		err_100_print(fd2);
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,15 +4,37 @@
 #define buf_size 1024
 
 /**
- * err_print - print error 99 for unable to write
+ * err_99_print - print error 99 for unable to write
  * @file: file accessed
  *
  * Return: nothing
  */
-void err_print(char **file)
+void err_99_print(char **file)
 {
 	dprintf(2, "Error: Can't write to %s\n", file[2]);
 	exit(99);
+}
+/**
+ * err_98_print - print error 98 for file not exist or can't read
+ * @file: file accessed
+ *
+ * Return: nothing
+ */
+void err_98_print(char **file)
+{
+		dprintf(2, "Can't read from file %s\n", file[1]);
+		exit(98);
+}
+/**
+ * err_100_print - print error 100 for unable to close
+ * @closer: fd value
+ *
+ * Return: nothing
+ */
+void err_100_print(int closer)
+{
+	dprintf(2, "Error can't close fd %d", closer);
+	exit(100);
 }
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
