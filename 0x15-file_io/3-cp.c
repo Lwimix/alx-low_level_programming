ba234a1d564910f82c835e96365f945c94353c43
Lwimix <martilwinx@gmail.com>
2023-05-08 22:52:50 +0300

    Another one

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -44,3 +44,38 @@ int main(int argc, char **argv)
 		err_100_print(fd2);
 	return (0);
 }
+/**
+ * err_98_print - print error 98 for file not exist or can't read
+ * @file: file accessed
+ *
+ * Return: nothing
+ */
+void err_98_print(char *file)
+{
+	dprintf(2, "Can't read from file %s\n", file);
+	exit(98);
+}
+
+/**
+ * err_99_print - print error 99 for unable to write
+ * @file: file accessed
+ *
+ * Return: nothing
+ */
+void err_99_print(char *file)
+{
+	dprintf(2, "Error: Can't write to %s\n", file);
+	exit(99);
+}
+
+/**
+ * err_100_print - print error 100 for unable to close
+ * @closer: fd value
+ *
+ * Return: nothing
+ */
+void err_100_print(int closer)
+{
+	dprintf(2, "Error can't close fd %d", closer);
+	exit(100);
+}
