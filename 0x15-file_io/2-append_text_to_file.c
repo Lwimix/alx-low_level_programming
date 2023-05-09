cc6b4a48128f7f4416bdeff7e1c21eb820a6fc72
Lwimix <martilwinx@gmail.com>
2023-05-09 03:00:06 +0300

    Other

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, num_char, leng, perm;
+	int fd, num_char, leng, perm, one_char;
 
 	perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	if (filename == NULL)
@@ -31,7 +31,12 @@ int append_text_to_file(const char *filename, char *text_content)
 			close(fd);
 			return (-1);
 		}
-		write(fd, "\n", 1);
+		one_char = write(fd, "\n", 1);
+		if (one_char == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 	close(fd);
 	return (1);
