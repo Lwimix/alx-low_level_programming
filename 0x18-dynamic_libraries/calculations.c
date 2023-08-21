3e1d0dfe55ab9e2b4c16be6eaa47d7bf21e96da6
Lwimix <martilwinx@gmail.com>
2023-08-21 13:29:33 +0300

    Cleaned

---
+++ 0x18-dynamic_libraries/calculations.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: addition of two numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: subtraction of two numbers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: multiplies two numbers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: division of two numbers
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulus of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: modulus division of two numbers
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
