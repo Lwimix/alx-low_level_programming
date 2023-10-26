b241f88ba6afa944c8756615af72aa0bf0fd9593
Lwimix <martilwinx@gmail.com>
2023-10-26 03:09:38 +0300

    Five on it

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This is island_perimeter
+It is a part of technical interview
+preparation
+"""
+def island_perimeter(grid):
+    """
+    The island_perimeter function
+    It calculates the perimeter of an
+    island in a grid
+    """
+    perim = 0
+    if grid != None:
+        size = 0
+        for small_grid in grid:
+            for item in small_grid:
+                if item == 1:
+                    size = size + 1
+        if size != 0:
+            perim = (size + 1) * 2
+    return perim
