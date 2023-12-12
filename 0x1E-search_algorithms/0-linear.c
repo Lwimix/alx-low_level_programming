9fbc15bd0805b2d62580b68a17244cddbfdef4be
Lwimix <martilwinx@gmail.com>
2023-12-13 00:42:15 +0300

    Searching

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * linear_search - loops from the first element until it finds the value
+ * @array: pointer to first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: first index where value is located
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array == NULL)
+		return (-1);
+	for (; i < size; i++)
+	{
+		if (value == array[i])
+			return ((int)i);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,2 @@
+**SEARCH ALGORITHMS**
+*Calculating space and time complexity of various search algorithms*
